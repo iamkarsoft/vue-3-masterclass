4db3a1fb1e22ce0d15c9246112600e0ea9f0ae33
Kofi Romain Amoussou <karsoft92@gmail.com>
2022-04-23 18:58:49 +0000

    automatically import components

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router.js'
-import AppDate from '@/components/AppDate.vue'
-
+// import AppDate from '@/components/AppDate.vue'
 const app = createApp(App)
 app.use(router)
-app.component('AppDate', AppDate)
+
+const requireComponent = require.context('./components', true, /App[A-Z]\w+\.(vue|js)$/)
+requireComponent.keys().forEach(function (fileName) {
+  let baseComponentConfig = requireComponent(fileName)
+  baseComponentConfig = baseComponentConfig.default || baseComponentConfig
+  const baseComponentName = baseComponentConfig.name || (
+    fileName
+      .replace(/^.+\//, '')
+      .replace(/\.\w+$/, '')
+  )
+  app.component(baseComponentName, baseComponentConfig)
+})
+// app.component('AppDate', AppDate)
 app.mount('#app')
