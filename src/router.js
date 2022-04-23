4219a0e426b760766f0847af2175760cfce9de4b
Kofi Romain Amoussou <karsoft92@gmail.com>
2022-04-23 14:41:39 +0000

    improve 404 and redirect when not found

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import PageHome from '@/components/PageHome.vue'
 import PageThreadShow from '@/components/PageThreadShow'
 import PageNotFound from '@/components/PageNotFound'
 import { createRouter, createWebHistory } from 'vue-router'
-
+import sourceData from '@/data.json'
 const routes = [
   {
     path: '/',
@@ -12,7 +12,24 @@ const routes = [
   {
     path: '/thread/:id',
     name: 'ThreadShow',
-    component: PageThreadShow
+    component: PageThreadShow,
+    props: true,
+    beforeEnter (to, from, next) {
+      // check if thread exists
+      const threadExist = sourceData.threads.find(
+        (thread) => thread.id === to.params.id
+      )
+
+      // continue navigation
+      if (threadExist) {
+        return next()
+      } else {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: this.$route.path.split('/') }
+        })
+      }
+    }
   },
   {
     path: '/:pathMatch(.*)*',
