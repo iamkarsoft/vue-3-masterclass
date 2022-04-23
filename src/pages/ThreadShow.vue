8a863026fac5f7043d263efe75fbc90c5b83847c
Kofi Romain Amoussou <karsoft92@gmail.com>
2022-04-23 17:50:50 +0000

    adding comments

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -1,12 +1,34 @@
 <template>
-  <div
-      class="col-large push-top"
-  >
+  <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
 
-    <post-list  :posts="threadPosts" />
-
+    <post-list :posts="threadPosts" />
+    <form action="" @submit.prevent="addPost">
+      <div class="form-group">
+        <!-- <label for="thread_title">Title:</label>
+        <input type="text" id="thread_title" class="form-input" name="title" /> -->
       </div>
+
+      <div class="form-group">
+        <label for="thread_content">Content:</label>
+        <textarea
+          id="thread_content"
+          class="form-input"
+          v-model="newPostText"
+          name="content"
+          rows="8"
+          cols="140"
+        ></textarea>
+      </div>
+
+      <div class="btn-group">
+        <button class="btn btn-ghost">Cancel</button>
+        <button class="btn btn-blue" type="submit" name="Publish">
+          Publish
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -26,20 +48,35 @@ export default {
   data () {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts
+      posts: sourceData.posts,
+      newPostText: ''
     }
   },
   computed: {
     thread () {
-      return this.threads.find(thread => thread.id === this.$route.params.id)
+      return this.threads.find((thread) => thread.id === this.$route.params.id)
     },
     threadPosts () {
-      return this.posts.filter(post => post.threadId === this.id)
+      return this.posts.filter((post) => post.threadId === this.id)
+    }
+  },
+  methods: {
+    addPost () {
+      const postId = 'ggg' + Math.random()
+      const post = {
+        id: postId,
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: '38St7Q8Zi2N1SPa5ahzssq9kbyp1'
+      }
+
+      this.posts.push(post)
+      this.thread.posts.push(postId)
     }
   }
 }
 </script>
 
 <style>
-
 </style>
