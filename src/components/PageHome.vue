7c6681e7882a77e09fcc0b52ad01496838906ec3
Kofi Romain Amoussou <karsoft92@gmail.com>
2022-04-23 02:01:25 +0000

    add threads and posts

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,10 +1,64 @@
 <template>
+  <div v-for="thread in threads"
+       :key="thread.id"
+       class="col-large push-top"
+  >
+    <h1>{{ thread.title }}</h1>
 
+    <div class="post-list">
+      <div class="post"
+           v-for="postId in thread.posts"
+           :key="postId"
+      >
+
+        <div class="user-info">
+          <a href="#" class="user-name">{{userById(postById(postId).userId).name}}</a>
+
+          <a href="#">
+            <img class="avatar-large" :src="userById(postById(postId).userId).avatar" alt="">
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+
+        </div>
+
+        <div class="post-content">
+          <div>
+            <p>
+              {{postById(postId).text}}
+            </p>
+          </div>
+        </div>
+
+        <div class="post-date text-faded">
+          {{postById(postId).publishedAt}}
+        </div>
+
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <script>
+import sourceData from '@/data.json'
 export default {
+  data () {
+    return {
+      threads: sourceData.threads,
+      users: sourceData.users,
+      posts: sourceData.posts
+    }
+  },
+  methods: {
+    postById (postId) {
+      return this.posts.find(post => post.id === postId)
+    },
 
+    userById (userId) {
+      return this.users.find(post => post.id === userId)
+    }
+  }
 }
 </script>
 
